9129cb6101920673c10b5fb9a7bc1eb25d261382
Angelika <angelika@sonic.net.pl>
2022-10-30 21:08:59 +0100

    Website optimization

--- src/app/_pages/for-clients/for-clients.component.ts
+++ src/app/_pages/for-clients/for-clients.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-for-clients',
   templateUrl: './for-clients.component.html',
   styleUrls: ['./for-clients.component.scss']
 })
-export class ForClientsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ForClientsComponent {}
